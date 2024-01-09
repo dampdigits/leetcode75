8fe6ade8f2b398eecefe11c1a3d0453499b892b3
dampdigits <dampdigits@gmail.com>
2024-01-09 22:13:46 +0530

    added comments to solution for problem-26

--- problem26/Solution.java
+++ problem26/Solution.java
@@ -4,19 +4,22 @@ class Solution {
 
 		for (int i = 0; i < s.length(); i++) {
 			char ch = s.charAt(i);
+			// Concat if letter
 			if (Character.isLetter(ch))
 				decodedStr += ch;
 			else if (Character.isDigit(ch)) {
 				int num = 0;
+				// Extract number
 				do {
 					num = (num * 10) + (ch - 48);
 					++i;
 					ch = s.charAt(i);
 				} while(Character.isDigit(ch));
+				// Extract substring to be repeated
 				int openBrkt = 0, closeBrkt = 0;
 				String substr = "";
 				++i;
-				while (closeBrkt <= openBrkt && i < s.length()) {
+				while (closeBrkt <= openBrkt) {
 					ch = s.charAt(i);
 					if (ch == '[')
 						openBrkt++;
@@ -25,6 +28,7 @@ class Solution {
 					substr += ch;
 					++i;
 				}
+				// Repeat substring and concat
 				decodedStr += new String(new char[num]).replace("\0", decodeString(substr));
 				i--;
 			}
