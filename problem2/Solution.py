af54c3b21dcf9e6fdd144fb9e538d4ce07f477ec
dampdigits <dampdigits@gmail.com>
2024-01-13 23:45:58 +0530

    optimised python solution for problem-2

--- problem2/Solution.py
+++ problem2/Solution.py
@@ -3,8 +3,4 @@ class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         if str1+str2 != str2+str1:
             return ""
-        hcf = gcd(len(str1), len(str2))
-        ans = str1[:hcf] if len(str1) < len(str2) else str2[:hcf]
-        if ans in str1 and ans in str2:
-            return ans
-        return ""
+        return str1[:gcd(len(str1), len(str2))]
