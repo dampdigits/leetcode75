646a85d24f532e8dc4636dffdcbede4cd666e0f7
dampdigits <dampdigits@gmail.com>
2024-02-12 14:27:21 +0530

    minor variable change

--- problem45/Solution.py
+++ problem45/Solution.py
@@ -1,29 +1,26 @@
 def minReorder(n, connections) -> int:
     edges = { (a,b) for a,b in connections}
-    print(f"edges:\n{edges}")
     neighbours = {city:[] for city in range(n)}
-    print(f"neighbours:\n{neighbours}")
-    visit = set()
+    visted = set()
     changes = 0
     
 
     for a,b in connections:
         neighbours[a].append(b)
         neighbours[b].append(a)
-    print(f"neighbours:\n{neighbours}")
 
     def dfs(city):
-        nonlocal edges, neighbours, visit, changes
+        nonlocal edges, neighbours, visted, changes
 
         for neighbour in neighbours[city]:
-            if neighbour in visit:
+            if neighbour in visted:
                 continue
             if (neighbour, city) not in edges:
                 changes += 1
-            visit.add(neighbour)
+            visted.add(neighbour)
             dfs(neighbour)
 
-    visit.add(0)
+    visted.add(0)
     dfs(0)
 
     return changes
