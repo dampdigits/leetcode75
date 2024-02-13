f85d073233f81990891a81a4d28ff673ea84c15d
dampdigits <dampdigits@gmail.com>
2024-02-14 00:48:31 +0530

    improved time complexity for java sol. in problem45
    replaced the local array visited with a global List

--- problem45/Solution.java
+++ problem45/Solution.java
@@ -3,33 +3,35 @@ import java.util.ArrayList;
 
 public class Solution {
     private int reorderCount = 0;
+    private List<Boolean> visited = new ArrayList<>();
 	private List<List<Integer>> graph = new ArrayList<>();
 
     public static void main(String[] args) {
-        int connections[][] = {{0,1},{1,3},{2,3},{4,0},{4,5}};
+        int connections[][] = {{1,0},{1,3},{2,3},{4,0},{4,5}};
         System.out.println("Reorders = " + new Solution().minReorder(6, connections));
     }
 
     public int minReorder(int n, int[][] connections) {
-		boolean[] visited = new boolean[n];
-		for (int i = 0; i < n; i++)
-			graph.add(i, new ArrayList<>());
+		for (int i = 0; i < n; i++) {
+            visited.add(i, false);
+            graph.add(i, new ArrayList<>());
+        }
 
 		for (int[] pair : connections) {
 			graph.get(pair[0]).add(pair[1]);
 			graph.get(pair[1]).add(-pair[0]);
 		}
-		dfs(0, visited);
+		dfs(0);
 		return reorderCount;
     }
 
-    private void dfs(int city, boolean[] visited) {
-		if (visited[city]) return;
-		visited[city] = true;
+    private void dfs(int city) {
+		if (visited.get(city)) return;
+		visited.set(city, true);
 		for (int neighbour : graph.get(city)) {
-			if (visited[Math.abs(neighbour)]) continue;
+			if (visited.get(Math.abs(neighbour))) continue;
 			if (neighbour > 0) reorderCount++;
-			dfs(Math.abs(neighbour), visited);
+			dfs(Math.abs(neighbour));
 		}
     }
 }
