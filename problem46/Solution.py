f05a581a7001c107a8d9836ac67a4297b50c5aa6
dampdigits <dampdigits@gmail.com>
2024-02-16 01:40:19 +0530

    create Solution.py for problem46

---
+++ problem46/Solution.py
@@ -0,0 +1,37 @@
+class Solution:
+    def calcEquation(self, equations, values, queries):
+        # Create a graph (dict of dicts)
+        graph = {}
+        for equation, value in zip(equations, values):
+            if equation[0] not in graph:
+                graph[equation[0]] = {}
+            if equation[1] not in graph:
+                graph[equation[1]] = {}
+            graph[equation[0]][equation[1]] = value
+            graph[equation[1]][equation[0]] = 1 / value
+        
+        visited, valStack = [], []
+
+        def dfs(node, target):
+            if node == target: return valStack.pop()
+            visited.append(node)
+            for key, val in graph[node].items():
+                if key in visited: continue
+                valStack.append(val)
+                value = dfs(key, target)
+                if value: return value * valStack.pop()
+            valStack.pop()
+            return 0
+
+        ans = []
+
+        for query in queries:
+            visited.clear()
+            valStack.append(1)
+            if query[0] not in graph or query[1] not in graph:
+                ans.append(-1)
+            else:
+                val = dfs(query[0], query[1])
+                ans.append(-1 if val == 0 else val)
+        
+        return ans
