a18e7a900fd504b8809cc0f99fcf82cd5dc4f988
dampdigits <dampdigits@gmail.com>
2024-02-25 03:31:57 +0530

    completed Solution.py for problem35

--- problem35/Solution.py
+++ problem35/Solution.py
@@ -6,4 +6,11 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        
+        def dfs(big, root):
+            if not root: return 0
+            isGood = 0
+            if root.val >= big:
+                isGood = 1
+                big = root.val
+            return isGood + dfs(big, root.left) + dfs(big, root.right)
+        return dfs(-10000, root)
