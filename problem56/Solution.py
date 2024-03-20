aae11acaec9a2a6f58623161a9b7965196696e59
dampdigits <dampdigits@gmail.com>
2024-03-20 15:58:42 +0530

    Finished solution for problem56

--- problem56/Solution.py
+++ problem56/Solution.py
@@ -1,3 +1,22 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-    
+        ans = max(piles)
+        if len(piles) == h: return ans
+        l, r = 1, ans
+        piles.sort(reverse=True)
+
+        while l <= r:
+            mid = l + ((r-l)>>1)
+            hrs = 0
+
+            for pile in piles:
+                hrs += ceil(pile / mid)
+                if hrs > h: break
+            
+            if hrs > h:
+                l = mid+1
+            else:
+                ans = mid
+                r = mid-1
+
+        return ans
