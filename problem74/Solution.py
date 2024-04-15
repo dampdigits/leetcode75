d1852d2b75d18406ab361e8a1063508bfca02939
dampdigits <dampdigits@gmail.com>
2024-04-15 23:28:58 +0530

    created solution for problem74

---
+++ problem74/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        answer = [0] * len(temperatures)
+
+        for i, temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temp:
+                index = stack.pop()
+                answer[index] = i - index
+            
+            stack.append(i)
+
+        return answer
